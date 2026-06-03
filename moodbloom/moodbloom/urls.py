1cbb4dcdde624d2e18ef2ed65720fe1bf840b411
Thai Xin Lin <xinlin007thai@gmail.com>
2026-06-03 14:31:55 +0800

    full system

--- moodbloom/moodbloom/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls), # 进入后台的路径
-    path('', include('mood.urls')), # 关键：这一行会把请求导向 mood 里的 urls.py
-]
