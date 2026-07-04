0377f3e37d78fa6d470818a2d05cf8397d48b55b
TAN MING HUUI <minghui010207@gmail.com>
2026-07-04 23:53:35 +0800

    typo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += staticfiles_urlpatterns()
-    if settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
